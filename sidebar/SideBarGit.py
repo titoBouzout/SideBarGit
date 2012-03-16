5f7995fff48312e89540991b11bac3e4d7550515
Tito Bouzout <tito.bouzout@gmail.com>
2012-03-16 07:48:48 -0300

    Add support for the new ok/cancel dialog

--- sidebar/SideBarGit.py
+++ sidebar/SideBarGit.py
@@ -241,9 +241,13 @@ class SideBarGit:
 		return True
 
 	def confirm(self, message, function, arg1):
-		import functools
-		sublime.active_window().run_command('hide_panel');
-		sublime.active_window().show_input_panel("Confirmation Required:", message.decode('utf-8'), functools.partial(function, arg1, True), None, None)
+		if int(sublime.version()) >= 2186:
+			if sublime.ok_cancel_dialog(u'Side Bar Git : '+message):
+				function(arg1, True)
+		else:
+			import functools
+			sublime.active_window().run_command('hide_panel');
+			sublime.active_window().show_input_panel("Confirmation Required:", message.decode('utf-8'), functools.partial(function, arg1, True), None, None)
 
 	def prompt(self, message, default, function, arg1):
 		import functools
